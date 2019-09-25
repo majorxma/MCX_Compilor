ef16fc7e6891023c08b3067c1bc5e45faa2e58c5
Mustang <majorxma@163.com>
2019-09-25 09:17:08 +0800

    homework2

--- LexicalAnalysis/Compiler_Lexical_Analysis/Compiler_Lexical_Analysis/Compiler_Lexical_Analysis.cpp
+++ LexicalAnalysis/Compiler_Lexical_Analysis/Compiler_Lexical_Analysis/Compiler_Lexical_Analysis.cpp
@@ -14,15 +14,30 @@ using namespace std;
 #define IS_NUMBER	2
 #define IS_CHAR		3
 #define IS_STRING	4
-#define IS_TK		5
+#define IS_OPERATOR	5
 #define IS_COMPARE	6
+#define IS_BRAKET   7
+#define IS_ASSIGN   8
+#define IS_COMMA    9
+#define IS_SEMICN   10
 
 //get the const for find out what your word is;
-
 FILE * fp;
 int line;
 string word;
 char c;
+string TKs[13][2] = { {"const", "CONSTTK"}, {"int", "INTTK"} , {"char", "CHARTK"}, 
+	{"void", "VOIDTK"}, {"main", "MAINTK"}, {"if", "IFTK"}, {"else", "ELSETK"}, 
+	{"do", "DOTK"}, {"while", "WHILETK"}, {"for", "FORTK"}, {"scanf", "SCANFTK"}, 
+	{"printf", "PRINTFTK"}, {"return", "RETURNTK"} };
+
+string OPERATORs[4][2] = { {"+", "PLUS"}, {"-", "MINU"}, {"*", "MULT"}, {"/", "DIV"} };
+
+string COMPAREs[6][2] = { {"<", "LSS"} , {"<=", "LEQ"} , {">", "GRE"} ,
+		{">=", "GEQ"} , {"==", "EQL"} , {"!=", "NEQ"} };
+
+string BRAKETs[6][2] = { {"(", "LPARENT"} , {")", "RPARENT"} , {"[", "LBRACK"} , 
+		{"]", "RBRACK"} , {"{", "LBRACE"} , {"}", "RBRACE"} };
 
 int getwords();
 void putwords(int a);
@@ -44,11 +59,64 @@ int main()
 }
 
 void putwords(int a) {
-
+	switch (a)
+	{
+	case IS_IDENFR:
+		for (int i = 0; i < 13; i++) {
+			if (word == TKs[i][0]) {
+				cout << TKs[i][1] << " " << word << endl;
+				return;
+			}
+		}
+		cout << "IDENFR" << " " << word << endl;
+		return;
+	case IS_NUMBER:
+		cout << "INTCON" << " " << word << endl;
+		return;
+	case IS_CHAR:
+		cout << "CHARCON" << " " << word << endl;
+		return;
+	case IS_STRING:
+		cout << "STRCON" << " " << word << endl;
+		return;
+	case IS_OPERATOR:
+		for (int i = 0; i < 4; i++) {
+			if (word == OPERATORs[i][0]) {
+				cout << OPERATORs[i][1] << " " << word << endl;
+				return;
+			}
+		}
+	case IS_COMPARE:
+		for (int i = 0; i < 6; i++) {
+			if (word == COMPAREs[i][0]) {
+				cout << COMPAREs[i][1] << " " << word << endl;
+				return;
+			}
+		}
+	case IS_BRAKET:
+		for (int i = 0; i < 6; i++) {
+			if (word == BRAKETs[i][0]) {
+				cout << BRAKETs[i][1] << " " << word << endl;
+				return;
+			}
+		}
+	case IS_ASSIGN:
+		cout << "ASSIGN" << " " << word << endl;
+		return;
+	case IS_COMMA:
+		cout << "COMMA" << " " << word << endl;
+		return;
+	case IS_SEMICN:
+		cout << "SEMICN" << " " << word << endl;
+		return;
+	default:
+		break;
+	}
+	
+	cout << word << endl;
 }
 
 int getwords() {
-	return -1;
 	word = "";
 	c = fgetc(fp);
 	while (c == ' ' || c == '	' || c == '\n') {
@@ -58,11 +126,10 @@ int getwords() {
 		return -1;
 	}
 	if (isalpha(c) || c == '_') {
-		while (isalpha(c) || c == '_') {
+		while ( c == '_' || isalnum(c)) {
 			word += c;
 			c = fgetc(fp);
 		}
-		cout << word << endl;
 		fseek(fp, -1, 1);
 		return IS_IDENFR;
 	}
@@ -71,22 +138,71 @@ int getwords() {
 			word += c;
 			c = fgetc(fp);
 		}
-		cout << word << endl;
 		fseek(fp, -1, 1);
 		return IS_NUMBER;
 	}
-	if (c == '+' || c == '-' || c == '*' || c == '/') {
-		return IS_TK;
+	if ( c == '+' || c == '-' || c == '*' || c == '/') {
+		word += c;
+		return IS_OPERATOR; 
 	}
-	if (c == '(' || c == ')' || c == '(' || c == '[' || c == ']' || c == '{' || c == '}') {
-		return 8;
+	if ( c == '(' || c == ')' || c == '(' || c == '[' || c == ']' || c == '{' || c == '}') {
+		word += c;
+		return IS_BRAKET;
+	}
+	if (c == ',') {
+		word += c;
+		return IS_COMMA;
+	}
+	if (c == ';') {
+		word += c;
+		return IS_SEMICN;
+	}
+	if ( c == '<' || c == '>') {
+		word += c;
+		c = fgetc(fp);
+		if ( c == '=') {
+			word += c;
+			return IS_COMPARE;
+		}
+		else {
+			fseek(fp, -1, 1);
+			return IS_COMPARE;
+		}
+	}
+	if (c == '=') {
+		word += c;
+		c = fgetc(fp);
+		if (c == '=') {
+			word += c;
+			return IS_COMPARE;
+		}
+		else {
+			fseek(fp, -1, 1);
+			return IS_ASSIGN;
+		}
+	}
+	if (c == '!') {
+		word += c;
+		c = fgetc(fp);
+		if (c == '=') {
+			word += c;
+			return IS_COMPARE;
+		}
+	}
+	if (c == '\'') {
+		c = fgetc(fp);
+		word += c;
+		c = fgetc(fp);
+		if (c == '\'') {
+			return IS_CHAR;
+		}
+	}
+	if (c == '\"') {
+		c = fgetc(fp);
+		while (c != '\"') {
+			word += c;
+			c = fgetc(fp);
+		}
+		return IS_STRING;
 	}
 }
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
